968993c54b586b60192b3f64b2865b02b20c987d
Shriyans28 <113304269+Shriyans28@users.noreply.github.com>
2023-06-17 17:18:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name = "Shriyans";
+var last_name = "Pothireddy";
+
+var a = 10;
+var b = 5;
+
+var name = first_name.concat(last_name);
+console.log("name");
+
+var add = a + b;
+console.log("add");
